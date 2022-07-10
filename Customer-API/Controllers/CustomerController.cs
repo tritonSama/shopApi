3e5065887522896fc73aa7cad78984f3d5129a51
tritonSama <blue-aquarius@live.com>
2022-07-09 19:52:01 -0500

    first

---
+++ Customer-API/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Customer>>> GetAllCustomer()
+        {
+            return new List<Customer>
+            {
+                new Customer
+                {
+                    firstName = "Josh",
+                    lastName = "Hen",
+
+                }
+            };
+        }
+
+    }
+}
---
+++ Customer-API/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Customer_API
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; } = String.Empty;
+        public string lastName { get; set; } = String.Empty;
+
+    }
+}
